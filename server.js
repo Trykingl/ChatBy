0510e0acbbb8c02dfca457884e21693f781fad72
fares <fares@ax.my.domain>
2026-08-30 16:57:45 +0100

    updated the database

--- server.js
+++ server.js
@@ -18,6 +18,24 @@ db.exec(`
   )
 `);
 
+app.get('/messages', (req, res) => {
+  const messages = db.prepare('SELECT * FROM messages ORDER BY id ASC').all();
+  res.json(messages);
+});
+
+app.post('messages', (req, res) => {
+  const { text, sender, time } = req.body;
+
+  if (!text || !sender || !time) {
+    return res.status(400).json({ error: 'Missing feilds' });
+  }
+
+  db.prepare('INSERT INTO messages (text, sender, time) VALUE (?, ?, ?)')
+    .run(text, sender, time);
+
+  res.sendStatus(201);
+});
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`server running at http://localhost:${PORT}`);
 })
