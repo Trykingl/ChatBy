6e78affcaffa981fed05c7975c5461fa82ab6e23
fares <fares@ax.my.domain>
2026-08-30 17:53:25 +0100

    added login

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Database = require('better-sqlite3');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const PORT = 3000;
@@ -17,6 +18,14 @@ db.exec(`
     time TEXT NOT NULL
   )
 `);
+db.exec(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+  )
+`);
+
 
 app.get('/messages', (req, res) => {
   const messages = db.prepare('SELECT * FROM messages ORDER BY id ASC').all();
@@ -34,6 +43,23 @@ app.post('/messages', (req, res) => {
     .run(text, sender, time);
 
   res.sendStatus(201);
+});
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'missing fields' });
+  }
+
+  const hashedpassword = await bcrypt.hash(password, 10);
+
+  try {
+    db.sendStatus('INSERT INTO users (username, password) VALUES (?, ?)')
+      .run(username, hashedpassword);
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(400).json({ error: 'Username already taken' });
+  }
 });
 
 app.listen(PORT, '0.0.0.0', () => {
